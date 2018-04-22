1c5e5e51c0056fc2b80b810dda1dc809bf65df29
mareq <mareq@balint.eu>
2018-04-22 17:59:03 +0100

    Soulution for assignment #04 (week 05)

--- assignments/machine-learning-ex4/ex4/nnCostFunction.m
+++ assignments/machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,22 +62,80 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% - eye(num_labels) creates identity matrix
+% - (y, :) select appropriate row (and with all its columns) from that
+%   identity matrix
+% - this is repeated for each value in `y` and the results are stacked
+%   as rows into the resulting `size(y,1)`x`num_labels` matrix (5000x10)
+Y = eye(num_labels)(y, :);
+% Thetas without bias (cut off the first column)
+Theta1WoBias = Theta1(:, 2:end);
+Theta2WoBias = Theta2(:, 2:end);
+
+% ============================================================================
+% Part 1: Forward Propagation and Cost Computation
+% ============================================================================
+
+% Input Layer
+X
+a1 = [ones(m, 1), X];
+
+% Hidden Layer
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+bias = ones(size(a2, 1), 1);
+a2 = [bias, a2];
+
+% Output Layer
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+% Cost Computation
+H = a3;
+% - both `H` and `Y` matrices have the same dimensions
+% - cost computation is done for each sample & class
+% - resulting costs are summed up (sum over samples, then sum over classes)
+J = (1 / m) * sum(sum((-Y .* log(H)) - ((1-Y) .* log(1-H))));
+
+% Regularization
+%r = (lambda / (2 * m)) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)));
+% - unroll Theta matrices into single columnar vector
+ThetasWoBias = [Theta1WoBias(:); Theta2WoBias(:)];
+% - run along the long horizontal vector and sum the squares ov the values
+r = (lambda / (2 * m)) * sum(ThetasWoBias' .^ 2);
+J += r;
 
 
+% ============================================================================
+% Part 2: Back Propagation
+% ============================================================================
 
+% Step 1: Forward Propagation
+% - already done in Part 1:
+% - activations for t-th sample: aN(t, :), zN(t, :)
 
+% Step 2: Delta for the Output Layer
+% - delta for t-th sample and k-th output unit: d3(t,k)
+d3 = a3 - Y;
 
+% Step 3: Delta for the Hidden Layer
+% - delta for t-th sample and k-th unit on hidden layer: d2(t,k)
+d2 = (d3 * Theta2WoBias) .* sigmoidGradient(z2);
 
+% Step 4: Accumulate Gradient
+D2 = d3' * a2;
+D1 = d2' * a1;
 
+% Step 5: Unregularized Gradient
+Theta1_grad = (1 / m) .* D1;
+Theta2_grad = (1 / m) .* D2;
 
+% ============================================================================
+% Part 2: Regularization
+% ============================================================================
 
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) += (lambda / m) * Theta1WoBias;
+Theta2_grad(:, 2:end) += (lambda / m) * Theta2WoBias;
 
 
 % -------------------------------------------------------------
